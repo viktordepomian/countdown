7bcaa68627f9a0c34b036b0dd981b37fb18f3f50
Viktor <90280932+viktordepomian@users.noreply.github.com>
2025-04-06 11:49:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Set the target date here (YYYY, MM-1, DD, HH, MM, SS)
 // Note: Month is 0-indexed (0 = January, 11 = December)
-const targetDate = new Date(2025, 4, 1, 12, 00, 00);
+// const targetDate = new Date(2025, 4, 1, 12, 00, 00);
+const targetDate = new Date(2250, 4, 1, 12, 00, 00);
 
 // DOM elements
 const weeksEl = document.getElementById('weeks');
